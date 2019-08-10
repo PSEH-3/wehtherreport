ae3c28e1fc286dad5e0b33b60c6505cc19372d23
PSEH-3 <atiwari15@sapient.com>
2019-08-10 16:23:10 +0530

    comment

--- src/main/java/com/whetherapp/WhetherappApplication.java
+++ src/main/java/com/whetherapp/WhetherappApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WhetherappApplication extends SpringBootServletInitializer{
+//public class WhetherappApplication extends SpringBootServletInitializer{
+public class WhetherappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhetherappApplication.class, args);
